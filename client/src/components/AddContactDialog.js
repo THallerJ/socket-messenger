423d9ddb91db261244a0e2e0be860b827e1f1dfa
Thomas Haller <hallert60@gmail.com>
2021-04-28 20:03:21 -0500

    added AddContactDialog

---
+++ client/src/components/AddContactDialog.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {
+	Button,
+	ButtonGroup,
+	Dialog,
+	DialogTitle,
+	Typography,
+	TextField,
+	Divider,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	divider: {
+		marginLeft: theme.spacing(2),
+		marginRight: theme.spacing(2),
+	},
+	textfields: {
+		display: 'flex',
+		flexDirection: 'column',
+		padding: theme.spacing(3),
+	},
+
+	idTextField: {
+		marginTop: theme.spacing(3),
+	},
+	buttonGroup: {
+		justifyContent: 'center',
+		paddingBottom: theme.spacing(2),
+	},
+}));
+
+const AddContactDialog = ({ open, setOpen }) => {
+	const classes = useStyles();
+
+	function handleSubmit() {
+		// add logic for adding contact
+		console.log('contact added');
+		setOpen(false);
+	}
+
+	return (
+		<Dialog open={open} onBackdropClick={() => setOpen(false)}>
+			<DialogTitle>
+				<div>
+					<Typography align="center" variant="h6">
+						Add Contact
+					</Typography>
+				</div>
+			</DialogTitle>
+			<Divider className={classes.divider} />
+			<div className={classes.textfields}>
+				<TextField
+					variant="outlined"
+					autoFocus
+					id="name"
+					label="Enter name"
+					type="name"
+				/>
+				<TextField
+					className={classes.idTextField}
+					variant="outlined"
+					id="ID"
+					label="Enter ID"
+					type="ID"
+				/>
+			</div>
+			<ButtonGroup className={classes.buttonGroup}>
+				<Button onClick={handleSubmit} variant="contained" color="primary">
+					OK
+				</Button>
+				<Button onClick={() => setOpen(false)} variant="contained">
+					Cancel
+				</Button>
+			</ButtonGroup>
+		</Dialog>
+	);
+};
+
+export default AddContactDialog;
--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import AddContactDialog from './AddContactDialog';
 
+const useStyles = makeStyles((theme) => ({
+	fab: {
+		position: 'absolute',
+		bottom: theme.spacing(2),
+		right: theme.spacing(5),
+	},
+}));
 const Contacts = () => {
-	return <div>Contacts</div>;
+	const classes = useStyles();
+	const [openDialog, setOpenDialog] = useState(false);
+
+	return (
+		<div>
+			<AddContactDialog open={openDialog} setOpen={setOpenDialog} />
+			<Fab
+				className={classes.fab}
+				onClick={() => setOpenDialog(true)}
+				color="primary"
+				aria-label="add"
+			>
+				<AddIcon />
+			</Fab>
+		</div>
+	);
 };
 
 export default Contacts;
