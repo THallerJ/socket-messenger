0e27774b72da2de13b176e401d17182004e40b56
Thomas Haller <hallert60@gmail.com>
2021-04-29 21:26:42 -0500

    contacts are now displayed

---
+++ client/src/components/Contact.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+	Grid,
+	Typography,
+	Card,
+	CardContent,
+	IconButton,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%',
+		marginLeft: theme.spacing(1),
+		marginRight: theme.spacing(1),
+		marginTop: theme.spacing(1),
+	},
+}));
+
+const Contact = ({ name, id }) => {
+	const classes = useStyles();
+
+	return (
+		<Card variant="outlined" className={classes.root}>
+			<CardContent>
+				<Grid container spacing={2}>
+					<Grid item xs={3} className={classes.name}>
+						<Typography noWrap>{name}</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography noWrap>{id}</Typography>
+					</Grid>
+					<Grid item xs={1}>
+						<IconButton onClick={() => console.log('delete contact')}>
+							<DeleteIcon />
+						</IconButton>
+					</Grid>
+				</Grid>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default Contact;
--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import AddContactDialog from './AddContactDialog';
+import Contact from './Contact';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
 	fab: {
@@ -15,8 +17,36 @@ const Contacts = () => {
 	const classes = useStyles();
 	const [openDialog, setOpenDialog] = useState(false);
 
+	const people = [
+		{
+			name: 'Tom',
+			id: '034c4b9d-1b6b-445d-89de-186c8b1abcc1',
+		},
+		{
+			name: 'Bill Hickensburg',
+			id: '034c4b9d-1b6b-445d-89de-186c8b1abcc1',
+		},
+		{
+			name: 'Steve',
+			id: '034c4b9d-1b6b-445d-89de-186c8b1abcc1',
+		},
+		{
+			name: 'Ryan',
+			id: '034c4b9d-1b6b-445d-89de-186c8b1abcc1',
+		},
+		{
+			name: 'Shelby',
+			id: '034c4b9d-1b6b-445d-89de-186c8b1abcc1',
+		},
+	];
+
 	return (
 		<div>
+			<Grid container alignItems="center" justify="center">
+				{people.map((person) => {
+					return <Contact name={person.name} id={person.id} />;
+				})}
+			</Grid>
 			<AddContactDialog open={openDialog} setOpen={setOpenDialog} />
 			<Fab
 				className={classes.fab}
