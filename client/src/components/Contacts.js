e22ac8da14e8a54496abd15006a5a46e9a43398c
halle189@umn.edu <hallert60@gmail.com>
2021-05-01 20:49:29 -0500

    a message is now shown in Contacts.js when no contacts are saved

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import AddContactDialog from './AddContactDialog';
-import Contact from './Contact';
-import { Grid } from '@material-ui/core';
-import ContactsHeader from './ContactsHeader';
-import { useContacts } from '../contexts/ContactsContext';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import AddContactDialog from "./AddContactDialog";
+import Contact from "./Contact";
+import { Grid, Typography } from "@material-ui/core";
+import ContactsHeader from "./ContactsHeader";
+import { useContacts } from "../contexts/ContactsContext";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		width: '100%',
+		width: "100%",
 	},
 	fab: {
-		position: 'absolute',
+		position: "absolute",
 		bottom: theme.spacing(2),
 		right: theme.spacing(5),
 	},
@@ -24,14 +24,39 @@ const Contacts = () => {
 
 	const { contacts } = useContacts();
 
-	return (
-		<div className={classes.root}>
+	const contactList = (
+		<div>
 			<ContactsHeader />
 			<Grid container alignItems="center" justify="center">
 				{contacts.map((person, index) => {
 					return <Contact key={index} name={person.name} id={person.id} />;
 				})}
 			</Grid>
+		</div>
+	);
+
+	const emptyContactList = (
+		<div>
+			<Grid
+				container
+				direction="column"
+				alignItems="center"
+				justify="center"
+				style={{ minHeight: "100vh" }}
+			>
+				<Typography color="textSecondary">
+					You don't have any contacts.
+				</Typography>
+				<Typography color="textSecondary">
+					Click the add symbol in the bottom right corner to add a contact.
+				</Typography>
+			</Grid>
+		</div>
+	);
+
+	return (
+		<div className={classes.root}>
+			{contacts.length > 0 ? contactList : emptyContactList}
 			<AddContactDialog open={openDialog} setOpen={setOpenDialog} />
 			<Fab
 				className={classes.fab}
