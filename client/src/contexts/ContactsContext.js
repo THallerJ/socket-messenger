683db4d3967a25107e92925120e427917d1529cd
Thomas Haller <hallert60@gmail.com>
2021-05-11 20:28:14 -0500

    modified idToName to return the id if the id does not map to a saved contact

--- client/src/contexts/ContactsContext.js
+++ client/src/contexts/ContactsContext.js
@@ -8,15 +8,12 @@ export const ContactsContextProvider = ({ children }) => {
 
 	// convert array of contact ids into an array of contact names
 	function idToName(contactList) {
-		return contactList.map((id) => {
-			return contacts
-				.filter((contact) => {
-					return contact.id === id;
-				})
-				.map((contact) => {
-					return contact.name;
-				});
-		});
+		return contactList
+			.map((id) => {
+				const result = contacts.find((contact) => contact.id === id);
+				return result !== undefined ? result : { name: id };
+			})
+			.map((contact) => contact.name);
 	}
 
 	const value = { contacts, setContacts, idToName };
