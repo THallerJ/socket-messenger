fb9d177a39cefed58815ca8ebc1c1b72f44783f1
Thomas Haller <hallert60@gmail.com>
2024-06-21 19:39:57 -0500

    updated docker compose and dbConfig to address error messages

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,23 +1,13 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const isProduction = process.env.IS_PRODUCTION == "true";
-
-const poolConfig = isProduction
-	? {
-			connectionString: process.env.DATABASE_URL,
-			ssl: {
-				required: true,
-				PromiseRejectionEvent: false,
-			},
-	  }
-	: {
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-			host: process.env.POSTGRES_HOST,
-			port: process.env.POSTGRES_PORT,
-			database: process.env.POSTGRES_DATABASE,
-	  };
+const poolConfig = {
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+	database: process.env.POSTGRES_DATABASE,
+};
 
 const pool = new Pool(poolConfig);
 
